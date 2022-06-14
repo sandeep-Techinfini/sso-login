d06c0a8596456259df2dd70ad09a04a4c166611d
Mr.Tikendra <mrtikendra@Mr.Tikendra>
2022-06-14 19:05:23 +0530

    getIdentityForUsernamePasswordCredentials

--- IDP_Service/service/index.js
+++ IDP_Service/service/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import {Level }from "level"; 
+import ttl from "level-ttl";
 import createCredentialsHandler from "./handlers/create-credentials";
+import exchangeCredentialsHandler from "./handlers/exchange-credentials";
 
 const app = express();
 app.locals.store = new Level("./store");
+app.locals.ttl = ttl(app.locals.store);
 
 app.post("/create-credentials", createCredentialsHandler);
+app.post("/exchange-credentials", exchangeCredentialsHandler);
 
 // IIFE so we don't need to define `port` as `let` ¯\_(ツ)_/¯
 const port = (() => {
